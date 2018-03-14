2a0ca3df8c454e1b120c6515c43b20c03e049fc7
Colton Gardner <ColtonBootCamp@DESKTOP-BBU8CVU>
2018-03-13 17:28:17 -0700

    Added the delete, and search functions. Search is using observables and piping the search terms to the heroservice.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,11 +22,15 @@ export class HeroesComponent implements OnInit {
   this.heroService.addHero({name} as Hero).subscribe(
     hero => {
       this.heroes.push(hero);
-    }
-  );
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
 
-}
 
   constructor(private heroService: HeroService) { }
 
